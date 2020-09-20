369f6fb9e281a1e7429b4b9c65cca07993369aea
Min Huang <h.min719@gmail.com>
2020-09-20 20:31:47 +0200

    [index.mdx] add content to expenseTracker

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,119 +1,128 @@
-import React, { useState } from 'react';
-import { Blog } from '../components';
-import { graphql, useStaticQuery, Link } from 'gatsby';
-import { RiSearchLine } from 'react-icons/ri';
+import React, { useState } from "react"
+import { Blog } from "../components"
+import { graphql, useStaticQuery, Link } from "gatsby"
+import { RiSearchLine } from "react-icons/ri"
 
 export const Blogs = ({ blogs, totalCount }) => {
-  const { 
-    allMdx: { nodes: recent }, 
-    codeCount: { totalCount: codeCount }, 
-    designCount: { totalCount: designCount }, 
+  const {
+    allMdx: { nodes: recent },
+    codeCount: { totalCount: codeCount },
+    designCount: { totalCount: designCount },
     lifeCount: { totalCount: lifeCount },
-  } = useStaticQuery(query);
+  } = useStaticQuery(query)
 
   const categories = [
-    { category: 'all', count: totalCount }, 
-    { category: 'code', count: codeCount }, 
-    { category: 'design', count: designCount }, 
-    { category: 'life', count: lifeCount }
-  ];
+    { category: "all", count: totalCount },
+    { category: "code", count: codeCount },
+    { category: "design", count: designCount },
+    { category: "life", count: lifeCount },
+  ]
 
   const [state, setState] = useState({
     filteredPosts: blogs,
-    query: '',
-  });
+    query: "",
+  })
 
-  const handleSelected = (selected) => (event) => {
-    const filteredPosts = blogs.filter(({ frontmatter: { categories }}) => (
-      selected === 'all' ? categories : categories.includes(selected)
-    ));
-    setState({ query: '', filteredPosts })
-  };
+  const handleSelected = selected => event => {
+    const filteredPosts = blogs.filter(({ frontmatter: { categories } }) =>
+      selected === "all" ? categories : categories.includes(selected)
+    )
+    setState({ query: "", filteredPosts })
+  }
 
-  const handleInputChange = (event) => {
-    const query = event.target.value;
-    const filteredPosts = blogs.filter(({ frontmatter: { title, description }, body }) => (
-      title.toLowerCase().includes(query.toLowerCase()) ||
-      description.toLowerCase().includes(query.toLowerCase()) ||
-      body.toLowerCase().includes(query.toLowerCase())
-    ));
-    setState({ query, filteredPosts });
-  };
+  const handleInputChange = event => {
+    const query = event.target.value
+    const filteredPosts = blogs.filter(
+      ({ frontmatter: { title, description }, body }) =>
+        title.toLowerCase().includes(query.toLowerCase()) ||
+        description.toLowerCase().includes(query.toLowerCase()) ||
+        body.toLowerCase().includes(query.toLowerCase())
+    )
+    setState({ query, filteredPosts })
+  }
 
   return (
-    <section className='section'>
-      <div className='section-center blogs-center'>
-        <div className='blogs-menu'>
+    <section className="section">
+      <div className="section-center blogs-center">
+        <div className="blogs-menu">
           <div>
             <h4>Search Posts</h4>
-            <div className='search-input'>
-              <input 
-                type='text' 
-                name='query' 
-                aria-label='Search'
-                placeholder='Search...' 
+            <div className="search-input">
+              <input
+                type="text"
+                name="query"
+                aria-label="Search"
+                placeholder="Search..."
                 onChange={handleInputChange}
               />
-              <RiSearchLine size='1.25rem' />
+              <RiSearchLine size="1.25rem" />
             </div>
           </div>
 
-          <div className='category'>
+          <div className="category">
             <h4>categories</h4>
-            <div className='category-list'>
-              {categories.map(({ category, count }) => (
-                <div key={category} style={{ display: 'flex', alignItems: 'center' }}>
-                  <button 
-                    className='category-name'
-                    onClick={handleSelected(category)}
-                  >
-                    {category}
-                  </button>
-                  <span className='category-count'>{`(${count})`}</span>
-                </div>
-              ))}
+            <div className="category-list">
+              {categories.map(
+                ({ category, count }) =>
+                  count !== 0 && (
+                    <div
+                      key={category}
+                      style={{ display: "flex", alignItems: "center" }}
+                    >
+                      <button
+                        className="category-name"
+                        onClick={handleSelected(category)}
+                      >
+                        {category}
+                      </button>
+                      <span className="category-count">{`(${count})`}</span>
+                    </div>
+                  )
+              )}
             </div>
           </div>
 
-          <div className='recent-block'>
+          <div className="recent-block">
             <h4>Featured Posts</h4>
-            <div className='recent-list'>
-              {recent.map(({ id, slug, frontmatter: { title, categories }}) => (
-                <Link key={id} to={`/blog/${slug}`}>
-                  <div className='recent-post'>
-                    {categories.map((category, index) => (
-                      <span className='blog-category' key={index}>
-                        {category}
-                      </span>
-                    ))}
-                    <h5>{title}</h5>
-                  </div>
-                </Link>
-              ))}
+            <div className="recent-list">
+              {recent.map(
+                ({ id, slug, frontmatter: { title, categories } }) => (
+                  <Link key={id} to={`/blog/${slug}`}>
+                    <div className="recent-post">
+                      {categories.map((category, index) => (
+                        <span className="blog-category" key={index}>
+                          {category}
+                        </span>
+                      ))}
+                      <h5>{title}</h5>
+                    </div>
+                  </Link>
+                )
+              )}
             </div>
           </div>
         </div>
 
-        {state.filteredPosts.length > 0
-          ? state.filteredPosts.map((post) => (<Blog key={post.id} {...post} />)
+        {state.filteredPosts.length > 0 ? (
+          state.filteredPosts.map(post => <Blog key={post.id} {...post} />)
         ) : (
-          <div className='notFound'>
+          <div className="notFound">
             <h1>No matching posts</h1>
           </div>
         )}
       </div>
     </section>
   )
-};
+}
 
 const query = graphql`
   {
     allMdx(
-      filter: { 
-        fileAbsolutePath: { regex: "/posts/" },
-        frontmatter: { featured: { eq: true }}
-      }, 
-      sort: { fields: frontmatter___date, order: DESC },
+      filter: {
+        fileAbsolutePath: { regex: "/posts/" }
+        frontmatter: { featured: { eq: true } }
+      }
+      sort: { fields: frontmatter___date, order: DESC }
       limit: 6
     ) {
       nodes {
@@ -125,32 +134,32 @@ const query = graphql`
           featured
         }
       }
-    },
+    }
     codeCount: allMdx(
       filter: {
-        fileAbsolutePath: { regex: "/posts/" }, 
-        frontmatter: { categories: { in: "code" }}
+        fileAbsolutePath: { regex: "/posts/" }
+        frontmatter: { categories: { in: "code" } }
       }
     ) {
       totalCount
-    },
+    }
     designCount: allMdx(
-      filter: { 
-        fileAbsolutePath: { regex: "/posts/" }, 
-        frontmatter: { categories: { in: "design" }}
+      filter: {
+        fileAbsolutePath: { regex: "/posts/" }
+        frontmatter: { categories: { in: "design" } }
       }
     ) {
       totalCount
-    },
+    }
     lifeCount: allMdx(
       filter: {
-        fileAbsolutePath: { regex: "/posts/" }, 
-        frontmatter: { categories: { in: "life" }}
+        fileAbsolutePath: { regex: "/posts/" }
+        frontmatter: { categories: { in: "life" } }
       }
     ) {
       totalCount
-    },
+    }
   }
 `
 
-export default Blogs;
+export default Blogs
