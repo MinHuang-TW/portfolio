e9c3b3362c626ebc90a88ca0d0f9c1c86ddecc9b
Min Huang <h.min719@gmail.com>
2020-10-22 17:42:36 +0200

    [404] update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { SEO, Layout} from '../components';
+import { SEO, Layout } from '../components';
+import { FaGhost } from 'react-icons/fa';
 
 const Error = () => (
   <Layout>
     <SEO title='Error' description='404 page' />
     <main className='error-page'>
-      <div className='error-container'>
-        <h1>Page does not exist.</h1>
+      <div className='blogs-main' style={{ margin: 0 }}>
+        <FaGhost />
+        <h1>Page does not exist</h1>
         <Link to='/' className='btn'>back home</Link>
       </div>
     </main>
