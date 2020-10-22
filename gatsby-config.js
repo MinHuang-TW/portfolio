41b736c138588f6240f358220b95c90c9aa607e2
Min Huang <h.min719@gmail.com>
2020-10-22 11:41:12 +0200

    [gatsby-config] strapi settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,8 +74,8 @@ module.exports = {
       resolve: `gatsby-source-strapi`,
       options: {
         apiURL: `http://localhost:1337`,
-        queryLimit: 1000, // Default to 100
-        contentTypes: [`jobs`, `projects`, `blogs`],
+        queryLimit: 100, // Default to 100
+        contentTypes: [`jobs`],
         singleTypes: [`about`],
       },
     },
--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,7 @@ export const query = graphql`
           projectStack
           image {
             childImageSharp {
-              fluid(maxWidth: 650, quality: 90) {
+              fluid(maxWidth: 650, quality: 72) {
                 ...GatsbyImageSharpFluid_withWebp
               }
             }
